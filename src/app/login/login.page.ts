f10186a8a1c6c2d54d6e33e043735c03482c8803
Sebsanchez1602 <seb.sanchez@duocuc.cl>
2022-09-23 14:45:37 -0300

    v2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
